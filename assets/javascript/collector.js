50385ea3cd17582cc842e432cc066ce046e7e547
EmilPatino <esp33@georgetown.edu>
2019-07-28 17:39:24 -0400

    Added person Firebase database to collection page, removed console.logs, added UI to record collection page

--- assets/javascript/collector.js
+++ assets/javascript/collector.js
@@ -8,13 +8,6 @@ $("#search-btn").on("click", function (event) {
 
     var sTitle = $("#title-input").val().trim();
     var sArtist = $("#artist-input").val().trim();
-    // var sGenre = $("#genre-input").val().trim();
-    // var sYear = $("#year-input").val().trim();
-
-    console.log(sTitle);
-    console.log(sArtist);
-    // console.log(sGenre);
-    // console.log(sYear);
 
     var queryURL = "https://api.discogs.com/database/search?release_title=" + sTitle + "&artist=" + sArtist +  "&format=vinyl&per_page=1&page=1&key=CQwYyYowOAghyybYpMYL&secret=hlsXvJXvbpgfeoNJJHhOjEmSWzBgKvnY";
 
@@ -22,22 +15,17 @@ $("#search-btn").on("click", function (event) {
       url: queryURL,
       method: "GET"
     }).then(function (response) {
-      console.log(response);
-      console.log(response.results[0].title);
       var imageResult = $("#image-result").attr("src", response.results[0].cover_image).addClass("limitPics");
       var titleResult = $("#title-result").text(response.results[0].title);
       var yearResult = $("#year-result").text(response.results[0].year);
       var genreResult = $("#genre-result").text(response.results[0].genre);
       var releaseID = response.results[0].id;
-      console.log(releaseID);
-
+      
       var queryURL = "https://api.discogs.com/marketplace/price_suggestions/" + releaseID + "?token=IxYjEFQzSJoGKBwroxGLmyBkwplVfHZgZkrcdxeg";
       $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function (response) {
-        console.log(response);
-
         var fairPriceResults = response['Fair (F)'].value;
         var fixedFairPrice = fairPriceResults.toFixed(2);
         $("#fair-price-result").text("$" + fixedFairPrice);
@@ -49,17 +37,16 @@ $("#search-btn").on("click", function (event) {
         var mintPriceResults = response['Mint (M)'].value;
         var fixedMintPrice = mintPriceResults.toFixed(2);
         $("#mint-price-result").text("$" + fixedMintPrice);
-
       });
     });
   });
 
   var config = {
-    apiKey: "AIzaSyATwSD8AZkiqB3og3ZR-FWpbZ_2zknjvfw",
-    authDomain: "collection-list.firebaseapp.com",
-    databaseURL: "https://collection-list.firebaseio.com/",
-    storageBucket: "collection-list.appspot.com",
-    messagingSenderId: "831848521683",
+    apiKey: "AIzaSyAo8ncgpdQXqUZvAXz8mAt11aoDf4-nZI0",
+    authDomain: "recordcollector-176f2.firebaseapp.com",
+    databaseURL: "https://recordcollector-176f2.firebaseio.com",
+    storageBucket: "recordcollector-176f2.appspot.com",
+    messagingSenderId: "956026125184",
 };
 
 firebase.initializeApp(config);
@@ -75,15 +62,12 @@ $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
-    console.log(response);
-
-    $("#collection-btn").on("click", function (event) {
+  $("#collection-btn").on("click", function (event) {
         event.preventDefault();
 
         var titleList = $("#title-input").val();
         var artistList = $("#artist-input").val();
         
-
         var newListing = {
             title: titleList,
             artist: artistList,
@@ -91,16 +75,12 @@ $.ajax({
 
         database.ref().push(newListing);
 
-        console.log(newListing.title);
-        console.log(newListing.artist);
-
         $("#title-input").val("");
         $("#artist-input").val("");
 
     });
 
     database.ref().on("child_added", function (childSnapshot) {
-        console.log(childSnapshot.val());
         var titleList = childSnapshot.val().title;
         var artistList = childSnapshot.val().artist;
         
@@ -127,18 +107,13 @@ $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function (response) {
-        console.log(response)
 
         var searchTitle = response.query.search[0].title;
         var searchSnippet = response.query.search[0].snippet;
         var pageId = response.query.search[0].pageid;
 
-        console.log(searchTitle);
-        console.log(searchSnippet);
-
         $("#aName").html('<a href="https://en.wikipedia.org/?curid=' + pageId + '">' + searchTitle + '<i>(wikipedia excerpt)</i></a>:');
         $("#snippet").html('...' + searchSnippet + '...');
-        console.log(queryURL);
       });
     });
   };
@@ -156,17 +131,13 @@ $.ajax({
         url: queryURL2,
         method: "GET"
       }).then(function (response) {
-        console.log(response)
 
         var searchTitle2 = response.query.search[0].title;
         var searchSnippet2 = response.query.search[0].snippet;
         var pageId2 = response.query.search[0].pageid;
 
-        console.log(searchTitle2);
-
         $("#artistName").html('<a href="https://en.wikipedia.org/?curid=' + pageId2 + '">' + searchTitle2 + '<i>(wikipedia excerpt)</i></a>:');
         $("#description-result").html('...' + searchSnippet2 + '...');
-        console.log(queryURL2);
 
       });
     });
