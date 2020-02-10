586040b7618aee8e80b1485d483916f052130113
EmilPatino <esp33@georgetown.edu>
2020-02-09 18:43:28 -0600

    Finally made the Pschic game mobile friendly by creating adding an array of buttons.

--- assets/javascript/psychic-game.js
+++ assets/javascript/psychic-game.js
@@ -1,3 +1,75 @@
+$(document).ready(function() {
+
+    // Initial array of letters to dynamically create buttons on the screen.
+    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    // Create a for-loop to iterate through the letters array.
+    for (var i = 0; i < letters.length; i++) {
+      // Create a variable named "letterBtn" equal to $("<button>");
+      var letterBtn = $("<button>");
+      // Add classes for CSS
+      letterBtn.addClass("letter-button letter letter-button-color");
+      // For each "letterBtn" a data-attribute called "data-letter".
+      letterBtn.attr("data-letter", letters[i]);
+      // For each "letterBtns" a text equal to "letters[i]".
+      letterBtn.text(letters[i]);
+      // Append each "letterBtn" to the "#buttons" div (provided).
+      $("#buttons").append(letterBtn);
+
+    }
+
+    // On click that captures what was pressed but that limits it to only letters.
+$(".letter-button").on("click", function() {
+    var userGuess = " " + ($(this).attr("data-letter")) + " ";
+            if (userGuess === " " + computerGuess + " ") {
+            wins++;
+            guesses.splice(0,9);
+            guessesLeft=9;
+            $("#success").empty();
+            $("#failure").empty();
+            var successPop = $("<div>");
+            successPop.addClass("alert alert-success alert-dismissible");
+            var successText = $("<a>");
+            successText.addClass("close")
+            successText.attr("data-dismiss", "alert");
+            successText.html("&times;");
+            var successDialog = $("<p>");
+            successDialog.text("You are one step closer to becoming the Psychic's apprentice.");
+            (successPop).append(successText);
+            (successPop).append(successDialog);
+            $("#success").append(successPop);
+            guessfunc();
+        } 
+        else {
+            guesses.push(userGuess);
+            guessesLeft--;
+        if ((guessesLeft === 0)) {
+            losses++;
+            guesses.splice(0,9);
+            guessesLeft=9;
+            guessfunc();
+            $("#success").empty();
+            $("#failure").empty();
+            var failurePop = $("<div>");
+            failurePop.addClass("alert alert-danger alert-dismissible");
+            var failureText = $("<a>");
+            failureText.addClass("close")
+            failureText.attr("data-dismiss", "alert");
+            failureText.html("&times;");
+            var failureDialog = $("<p>");
+            failureDialog.text("The Psychic sensed you didn't have the sight...");
+            (failurePop).append(failureText);
+            (failurePop).append(failureDialog);
+            $("#failure").append(failurePop);
+            }
+        }
+            winsText.textContent = " " + wins;
+            lossesText.textContent = " " + losses;
+            guessesText.textContent = " " + guesses;
+            guessesLeftText.textContent = " " + guessesLeft;
+          });
+  });
+
+
 function myFunction() {
     $("#myForm")[0].reset();
   }
@@ -91,4 +163,3 @@ var userGuess = " " + event.key + " ";
         guessesText.textContent = " " + guesses;
         guessesLeftText.textContent = " " + guessesLeft;
       };
-
